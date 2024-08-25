275ab3f88b566b1d9bf2524f65722e70d1c88cca
Shahar <shaharariel95@gmail.com>
2024-08-25 21:13:26 +0300

    absulute path

--- src/PdfReader.tsx
+++ src/PdfReader.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './PdfReader.css';
-import pdfFile from './assets/cv.pdf';
+// import pdfFile from './assets/cv.pdf';
 
 const PdfReader: React.FC = () => {
   return (
     <div className="pdf-reader">
-      <iframe src={pdfFile} width="100%" height="100%" title="cv"></iframe>
+      <iframe src='/assets/cv.pdf' width="100%" height="100%" title="cv"></iframe>
     </div>
   );
 };
