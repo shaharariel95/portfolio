aba19b16f639785e9ebfe9d66c39e0a882ed0534
Shahar <shaharariel95@gmail.com>
2024-08-25 21:07:50 +0300

    CV to cv

--- src/PdfReader.tsx
+++ src/PdfReader.tsx
@@ -5,7 +5,7 @@ import pdfFile from './assets/cv.pdf';
 const PdfReader: React.FC = () => {
   return (
     <div className="pdf-reader">
-      <iframe src={pdfFile} width="100%" height="100%" title="CV"></iframe>
+      <iframe src={pdfFile} width="100%" height="100%" title="cv"></iframe>
     </div>
   );
 };
